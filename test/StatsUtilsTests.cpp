5e9f940d7d5a1f54a7c2ed639544a8697d92815f
Dutchworth <dutchworth@gmail.com>
2017-04-28 16:37:54 +0100

    StatsUtils functions  - factorial function  - combinations of subsets of given size of set

---
+++ test/StatsUtilsTests.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include "StatsUtils.h"
+#include <cmath>
+
+class StatsUtilsTests : public ::testing::Test {
+private:
+
+  void empty2dVect(std::vector<std::vector<int> >& vect) {
+    vect.clear();
+  }
+
+protected:
+
+  std::vector<int>nums4 { { 1, 2, 3, 4 } };
+  std::vector<std::vector<int> >combs4to3;
+
+  virtual void SetUp() {
+    combs4to3.push_back(std::vector<int>({ 1, 2, 3 }));
+    combs4to3.push_back(std::vector<int>({ 1, 2, 4 }));
+    combs4to3.push_back(std::vector<int>({ 1, 3, 4 }));
+    combs4to3.push_back(std::vector<int>({ 2, 3, 4 }));
+  }
+
+  virtual void TearDown() {
+    empty2dVect(combs4to3);
+  }
+};
+
+TEST_F(StatsUtilsTests, testFactorial) {
+  for (unsigned long long i = 0; i <= 6; ++i) {
+    unsigned long long expected = std::tgamma(i + 1) + 0.5; // extra 0.5 to
+                                                            // force proper
+                                                            // rounding
+    unsigned long long actual   = StatsUtils::factorial(i);
+    EXPECT_EQ(expected, actual);
+  }
+}
+
+TEST_F(StatsUtilsTests, testGetCombinations) {
+  std::vector<std::vector<int> > new4C3 = StatsUtils::getCombinations(nums4, 3);
+
+  EXPECT_EQ(combs4to3, new4C3);
+}
