a4ba7b38cef49931080c36dda72e1a938778a48d
Dutchworth <dutchworth@gmail.com>
2017-04-28 16:55:01 +0100

    finished combinations of 4 tests

--- test/StatsUtilsTests.cpp
+++ test/StatsUtilsTests.cpp
@@ -12,13 +12,31 @@ private:
 protected:
 
   std::vector<int>nums4 { { 1, 2, 3, 4 } };
+
+  std::vector<std::vector<int> >combs4to1;
+  std::vector<std::vector<int> >combs4to2;
   std::vector<std::vector<int> >combs4to3;
+  std::vector<std::vector<int> >combs4to4;
 
   virtual void SetUp() {
+    combs4to1.push_back(std::vector<int>({ 1 }));
+    combs4to1.push_back(std::vector<int>({ 2 }));
+    combs4to1.push_back(std::vector<int>({ 3 }));
+    combs4to1.push_back(std::vector<int>({ 4 }));
+
+    combs4to2.push_back(std::vector<int>({ 1, 2 }));
+    combs4to2.push_back(std::vector<int>({ 1, 3 }));
+    combs4to2.push_back(std::vector<int>({ 1, 4 }));
+    combs4to2.push_back(std::vector<int>({ 2, 3 }));
+    combs4to2.push_back(std::vector<int>({ 2, 4 }));
+    combs4to2.push_back(std::vector<int>({ 3, 4 }));
+
     combs4to3.push_back(std::vector<int>({ 1, 2, 3 }));
     combs4to3.push_back(std::vector<int>({ 1, 2, 4 }));
     combs4to3.push_back(std::vector<int>({ 1, 3, 4 }));
     combs4to3.push_back(std::vector<int>({ 2, 3, 4 }));
+
+    combs4to4.push_back(std::vector<int>({ 1, 2, 3, 4 }));
   }
 
   virtual void TearDown() {
@@ -34,11 +52,20 @@ TEST_F(StatsUtilsTests, testFactorial) {
     EXPECT_EQ(expected, actual);
   }
 
+  EXPECT_EQ(1, StatsUtils::factorial(0));
   EXPECT_EQ(1, StatsUtils::factorial(-1));
 }
 
 TEST_F(StatsUtilsTests, testGetCombinations) {
-  std::vector<std::vector<int> > new4C3 = StatsUtils::getCombinations(nums4, 3);
+  std::vector<std::vector<int> > new4C1 = StatsUtils::getCombinations(nums4, 1);
+  EXPECT_EQ(combs4to1, new4C1);
 
+  std::vector<std::vector<int> > new4C2 = StatsUtils::getCombinations(nums4, 2);
+  EXPECT_EQ(combs4to2, new4C2);
+
+  std::vector<std::vector<int> > new4C3 = StatsUtils::getCombinations(nums4, 3);
   EXPECT_EQ(combs4to3, new4C3);
+
+  std::vector<std::vector<int> > new4C4 = StatsUtils::getCombinations(nums4, 4);
+  EXPECT_EQ(combs4to4, new4C4);
 }
