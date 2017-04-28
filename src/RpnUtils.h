80baefc4629781ad1a4510585a01e8adda96d647
Dutchworth <dutchworth@gmail.com>
2017-04-28 16:36:16 +0100

    change RpnUtils to functions in namespace (as oppsoed to static methods of class)

--- src/RpnUtils.h
+++ src/RpnUtils.h
@@ -6,22 +6,22 @@
 #include <vector>
 #include <string>
 
-class RpnUtils {
-private:
+namespace RpnUtils {
+// converts the given stack to a vector for easier print and manipulation
+std::vector<Element>convertToVect(std::stack<Element>stack);
 
-  static std::vector<Element>convertToVect(std::stack<Element>stack);
+// returns the evaluated value of the given stack of emelents int rpn format
+int                 evaluateStackRecursive(std::stack<Element>& stack);
 
-  // returns the evaluated value of the given stack of emelents int rpn format
-  static int                 evaluateStackRecursive(std::stack<Element>& stack);
+// returns the evaluated value of the given stack of elements int rpn format.
+// first checks if it's a valid rpn stack.
+int         evaluateStack(std::stack<Element>stack);
 
-public:
+// returns whether the given stack of elements is a valid rpn stack
+bool        isValidStack(std::stack<Element>stack);
 
-  static int         evaluateStack(std::stack<Element>stack);
-
-  // returns whether the given stack of elements is a valid rpn stack
-  static bool        isValidStack(std::stack<Element>stack);
-
-  static std::string to_string(std::stack<Element>stack);
-};
+// converts the given rpn stack to a string for printing
+std::string to_string(std::stack<Element>stack);
+}
 
 #endif // ifndef RPNUTILS_H
