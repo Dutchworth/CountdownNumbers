2470b2cb806ac6b77b1d7ead9febacf8a40ff569
Dutchworth <dutchworth@gmail.com>
2017-04-28 09:52:52 +0100

    Unit tests of Element class

--- test/ElementTests.cpp
+++ test/ElementTests.cpp
@@ -17,7 +17,76 @@ protected:
 };
 
 TEST_F(ElementTests, testEmptyConstructor) {
+  Element newElement1;
+
+  EXPECT_EQ(newElement1, element1);
   EXPECT_TRUE(element1.isNumber());
   EXPECT_EQ(0,      element1.getValue());
   EXPECT_EQ(NUMBER, element1.getOperation());
+}
+
+TEST_F(ElementTests, testIntConstructor) {
+  Element newElement2(10);
+
+  EXPECT_EQ(newElement2, element2);
+  EXPECT_TRUE(element2.isNumber());
+  EXPECT_EQ(10,     element2.getValue());
+  EXPECT_EQ(NUMBER, element2.getOperation());
+}
+
+TEST_F(ElementTests, testOperationConstructor) {
+  Element newElement3(PLUS);
+
+  EXPECT_EQ(newElement3, element3);
+  EXPECT_FALSE(element3.isNumber());
+  EXPECT_EQ(0,    element3.getValue());
+  EXPECT_EQ(PLUS, element3.getOperation());
+}
+
+TEST_F(ElementTests, testCopyConstrustor) {
+  Element newElement1(element1);
+  Element newElement2(element2);
+  Element newElement3(element3);
+
+  EXPECT_EQ(newElement1, element1);
+  EXPECT_EQ(newElement2, element2);
+  EXPECT_EQ(newElement3, element3);
+}
+
+TEST_F(ElementTests, testAssignmentOperator) {
+  Element newElement1 = element1;
+  Element newElement2 = element2;
+  Element newElement3 = element3;
+
+  EXPECT_EQ(newElement1, element1);
+  EXPECT_EQ(newElement2, element2);
+  EXPECT_EQ(newElement3, element3);
+}
+
+TEST_F(ElementTests, testGetValue) {
+  EXPECT_EQ(0,  element1.getValue());
+  EXPECT_EQ(10, element2.getValue());
+  EXPECT_EQ(0,  element3.getValue());
+}
+
+TEST_F(ElementTests, testGetOperation) {
+  EXPECT_EQ(NUMBER, element1.getOperation());
+  EXPECT_EQ(NUMBER, element2.getOperation());
+  EXPECT_EQ(PLUS,   element3.getOperation());
+}
+
+TEST_F(ElementTests, testIsNumber) {
+  EXPECT_TRUE(element1.isNumber());
+  EXPECT_TRUE(element2.isNumber());
+  EXPECT_FALSE(element3.isNumber());
+}
+
+TEST_F(ElementTests, testGetString) {
+  std::string expected1 = "0";
+  std::string expected2 = "10";
+  std::string expected3 = "+";
+
+  EXPECT_EQ(expected1, element1.getString());
+  EXPECT_EQ(expected2, element2.getString());
+  EXPECT_EQ(expected3, element3.getString());
 }
