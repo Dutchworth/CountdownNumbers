e80293dd19826187850ba4b0d6bb380484b7ce17
Dutchworth <dutchworth@gmail.com>
2017-05-04 15:36:46 +0100

    Refactor SolverTests to test against solutions and better naming

--- test/SolverTests.cpp
+++ test/SolverTests.cpp
@@ -28,18 +28,60 @@ TEST_F(SolverTests, testSolveSimple) {
   EXPECT_EQ(expected, actual);
 }
 
-TEST_F(SolverTests, testSolveHard) {
+TEST_F(SolverTests, testSolve2Large) {
   std::vector<int> input { { 100, 75, 4, 5, 2, 1 } };
   int target = 870;
 
-  std::vector<Element> solution;
-  EXPECT_TRUE(Solver::solve(input, target, solution));
+  // 5 * (75 - 1 + 100)
+  std::vector<Element> expected { { Element(5),
+                                    Element(75),
+                                    Element(1),
+                                    Element(MINUS),
+                                    Element(100),
+                                    Element(PLUS),
+                                    Element(MULTIPLY) } };
+  std::vector<Element> actual;
+  EXPECT_TRUE(Solver::solve(input, target, actual));
+  EXPECT_EQ(expected, actual);
 }
 
-TEST_F(SolverTests, testSolveHarder) {
+TEST_F(SolverTests, testSolve4Large) {
   std::vector<int> input { { 50, 25, 100, 75, 8, 7 } };
   int target = 252;
 
-  std::vector<Element> solution;
-  EXPECT_TRUE(Solver::solve(input, target, solution));
+  // 7 * (8 + 100) * 25 / 75
+  std::vector<Element> expected { { Element(7),
+                                    Element(8),
+                                    Element(100),
+                                    Element(PLUS),
+                                    Element(25),
+                                    Element(MULTIPLY),
+                                    Element(75),
+                                    Element(DIVIDE),
+                                    Element(MULTIPLY) } };
+  std::vector<Element> actual;
+  EXPECT_TRUE(Solver::solve(input, target, actual));
+  EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SolverTests, testSolveNoLarge) {
+  std::vector<int> input { { 8, 7, 2, 1, 10, 1 } };
+  int target = 832;
+
+  // (1 + 2 + 10) * (1 + 7) * 8
+  std::vector<Element> expected { { Element(1),
+                                    Element(2),
+                                    Element(10),
+                                    Element(PLUS),
+                                    Element(PLUS),
+                                    Element(1),
+                                    Element(7),
+                                    Element(PLUS),
+                                    Element(8),
+                                    Element(MULTIPLY),
+                                    Element(MULTIPLY) } };
+  std::vector<Element> actual;
+
+  EXPECT_TRUE(Solver::solve(input, target, actual));
+  EXPECT_EQ(expected, actual);
 }
